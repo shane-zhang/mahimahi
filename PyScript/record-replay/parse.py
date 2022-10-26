c9cfe2f0077fd2df79d0bed4b881cb5fc7a2dc03
Shane Zhang <zx0319@gmail.com>
2022-10-26 14:07:31 -0400

    Fix the network interface name, add the dir struction

--- PyScript/record-replay/parse.py
+++ PyScript/record-replay/parse.py
@@ -35,7 +35,7 @@ def parse_pkt(pkt):
     return (flags, time, src, dst)
 
 def is_private(address):
-    return address == '172.31.4.182'
+    return address == '10.0.2.15'
     # return address.split('.')[0] == '10' and address.split('.')[1] == '0'
 
 def ping(ip):
